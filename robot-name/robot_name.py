acbbea35ecd07b89bd19696c29328e873042fbe1
gadget <road.runner.grach@gmail.com>
2019-03-27 23:39:58 +0300

    Add part of logic for solution on exercise robot-name

--- robot-name/robot_name.py
+++ robot-name/robot_name.py
@@ -6,12 +6,22 @@ robot_names = []
 
 class Robot(object):
     def __init__(self):
-        self.name = self.get_robot_name()
+        self.name = self.get_valid_name()
 
-    def get_robot_name(self, char_length=3, number_length=3):
+    def reset(self):
+        self.name = self.get_valid_name()
+
+    def get_valid_name(self):
+        name_in_list = True
+        robot_name = ''
+        while name_in_list:
+            robot_name = self.get_name()
+            if robot_name not in robot_names:
+                name_in_list = False
+        return robot_name
+
+    def get_name(self, char_length=3, number_length=3):
         res = ''.join(random.choice(string.ascii_uppercase) for i in range(char_length)) + \
                ''.join(random.choice(string.digits) for i in range(number_length))
         return res
 
-    def reset(self):
-        self.name = self.get_robot_name()
