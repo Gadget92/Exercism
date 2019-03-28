cb3a2fc6d7c17e409fdaf01ea6ab002553292c3b
gadget <road.runner.grach@gmail.com>
2019-03-29 00:35:56 +0300

    Complete exercise

--- robot-name/robot_name.py
+++ robot-name/robot_name.py
@@ -1,10 +1,10 @@
 import string
 import random
 
-robot_names = []
-
 
 class Robot(object):
+    robot_names = []
+
     def __init__(self):
         self.name = self.get_valid_name()
 
@@ -12,15 +12,15 @@ class Robot(object):
         self.name = self.get_valid_name()
 
     def get_valid_name(self):
-        name_in_list = True
         robot_name = ''
-        while name_in_list:
+        while True:
             robot_name = self.get_name()
-            if robot_name not in robot_names:
-                name_in_list = False
+            if robot_name not in self.robot_names:
+               self.robot_names.append(robot_name) 
+               break
         return robot_name
 
-    def get_name(self, char_length=3, number_length=3):
+    def get_name(self, char_length=2, number_length=3):
         res = ''.join(random.choice(string.ascii_uppercase) for i in range(char_length)) + \
                ''.join(random.choice(string.digits) for i in range(number_length))
         return res
