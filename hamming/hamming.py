9fdc18ef94fdb22d31c5acc7554604fde3ab7928
gadget <road.runner.grach@gmail.com>
2019-04-03 18:43:06 +0300

    Rewrite solution

--- hamming/hamming.py
+++ hamming/hamming.py
@@ -1,10 +1,5 @@
 def distance(strand_a, strand_b):
     if len(strand_a) != len(strand_b):
         raise ValueError('Variable not equals!')
-    else:
-        dist = 0
-        zipped = zip(strand_a, strand_b)
-        for a,b in list(zipped):
-            if a != b:
-                dist += 1
-        return dist
+
+    return sum(a != b for a, b in zip(strand_a, strand_b))
